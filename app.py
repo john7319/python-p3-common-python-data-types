f2a54d7a71daa8524bcd074632042a6992453ac7
john7319 <jonniekaras@gmail.com>
2024-05-20 12:11:36 +0300

    performing personal studies

---
+++ app.py
@@ -0,0 +1,15 @@
+# You can also create a new string by using the built-in str() constructor function (though it's not common you'd need to):
+
+str("I'm a string")
+
+
+dog_name = "Lucy"
+print(f"Say hello to my dog {dog_name}")
+
+price_1 = 3
+price_2 = 2.5
+
+print(f"Item 1 costs ${price_1:.2f}")
+# => Item 1 costs $3.00
+print(f"Item 2 costs ${price_2:.2f}")
+# => Item 2 costs $2.50
